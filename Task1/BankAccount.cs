fb102acedc1e4739bdb3aa5e931260e9cf309cc7
alexey02rus <101921891+alexey02rus@users.noreply.github.com>
2022-08-21 18:52:06 +0300

    Добавьте файлы проекта.

---
+++ Task1/BankAccount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class BankAccount<T>
+    {
+        T number;
+        double balance;
+        string name;
+
+        public void SetInfo()
+        {
+            bool beBalance;
+            Console.WriteLine("Введите номер счета");
+            number = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+            Console.WriteLine("Введите ФИО");
+            name = Console.ReadLine();
+            do
+            {
+                Console.WriteLine("Введите баланс");
+                beBalance = double.TryParse(Console.ReadLine(), out balance);
+                if (!beBalance)
+                {
+                    Console.WriteLine("Ошибка! Неверный формат данных. Попробуйте ещё раз.");
+                }
+            } while (!beBalance);
+        }
+        public string GetInfo()
+        {
+            return $"Счет номер: {number};\nФИО: {name};\nБаланс: {balance}";
+        }
+
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount<int> bankAccount1 = new BankAccount<int>();
+            bankAccount1.SetInfo();
+            Console.WriteLine(bankAccount1.GetInfo());
+
+            BankAccount<string> bankAccount2 = new BankAccount<string>();
+            bankAccount2.SetInfo();
+            Console.WriteLine(bankAccount2.GetInfo());
+
+            Console.ReadKey();
+        }
+    }
+}
